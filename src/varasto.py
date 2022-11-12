7e709f2d806a907c1b6876afe59ec7be1483f0b1
Joonas <KA0Screativity@gmail.com>
2022-11-12 23:42:20 +0200

    Vk 2, Harjoitus 3, pylint virheet korjattu, korjattu myös edellisellä viikolla tehty virhe konstruktorissa kun en tiennyt, että alku_saldo=0.0 asettaa oletusarvon Jos arvoa ei ole annettu

--- src/varasto.py
+++ src/varasto.py
@@ -1,10 +1,6 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+    def __init__(self, tilavuus, alku_saldo=0.0):
+        self.tilavuus = max(tilavuus, 0.0)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -16,18 +12,10 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = self.tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
-        if self.tilavuus > 0: #tänne lisätty turhia lohkoja jotta nested blocks sääntö rikkoutuu
-            if self.saldo > 0:
-                if self.tilavuus > self.saldo:
-                    return self.tilavuus - self.saldo
-                else:
-                    return 0.0
-            else:
-                return self.tilavuus
-        else:
-            return 0
+        return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
         if maara < 0:
@@ -48,19 +36,6 @@ class Varasto:
 
         self.saldo = self.saldo - maara
 
-        eka = 4 #lisätty rikkomaan max statements
-        toka = 3
-        kolmas = 2
-        summa = eka + toka
-        summa += kolmas
-        print(summa)
-
-        lista =[]
-        lista.append(eka)
-        lista.append(toka)
-        lista.append(kolmas)
-
-        print(max(lista))
         return maara
 
     def __str__(self):
